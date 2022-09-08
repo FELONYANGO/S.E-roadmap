00efadf1fc7e497b768ca15be97493f6a8b09fd6
FELONYANGO <felixonyango951@gmail.com>
2022-09-08 16:42:07 +0300

    project one
    finalized practice

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const color = document.querySelector(".color");
 btn.addEventListener("click",function(){
     //get a random number between the given array
     const randomNumber = getRandomNumber();
-console.log(randomNumber);
+//console.log(randomNumber);
 
     document.body.style.backgroundColor =colors[randomNumber];
     color.textContent = colors[randomNumber];
--- hex.js
+++ hex.js
@@ -1,7 +1,27 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 //get the button
-const hexx=document.getElementById('btn');
+const btn=document.getElementById('btn');
 //select the colors
-const color=document.querySelector('color');
+const color=document.querySelector('.color');
+
+//function that is used to iterate within the hexcode on button onclick
+btn.addEventListener("click",function(){
+   ///loop through array of numebers given
+   let hexColor = "#"; 
+    for(let i =0 ;i<6 ;i++){
+        hexColor +=hex[getRandomNumber()];
+    }
+
+//    const randomNumber=getHexcode();
+   color.textContent = hexColor;
+   document.body.style.backgroundColor = hexColor;
+  
+   
+});
+//the getHexcode function
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
+
 
