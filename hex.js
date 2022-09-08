66d3e1765e988fb3d97d37d874ade8f4f9a1ba6f
FELONYANGO <felixonyango951@gmail.com>
2022-09-08 13:34:55 +0300

    first commit
    project one color flipper

---
+++ hex.js
@@ -0,0 +1,7 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+//get the button
+const hexx=document.getElementById('btn');
+//select the colors
+const color=document.querySelector('color');
+
